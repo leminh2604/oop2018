6f2fd039d7e45c5221c3f65e2c8b7cab4e440e3e
leminh2604 <43198814+leminh2604@users.noreply.github.com>
2018-09-17 21:14:48 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,72 @@
+package week1;
+
+public class Student {
+
+    // TODO: khai báo các thuộc tính cho Student
+	private String name ,id ,group ,email;
+
+    // TODO: khai báo các phương thức getter, setter cho Student
+	
+	public void setName(String name1){
+		this.name = name1;}
+	public String getName(){
+		return name;}	
+	public void setId(String id1){
+		this.id = id1;}
+	public String getId(){	
+		return id;}
+	public void  setGroup(String group1){
+		this.group = group1;}
+	public String getGroup(){
+		return group;}
+	public void getEmail(String email1){
+		this.email=email1;}
+	public String getEmail(){
+		return email;}
+    /**
+     * Constructor 1
+     */
+	public Student() {
+		name="Student";
+		id="000";
+		group="INT22041";
+		email="uet@vnu.edu.vn";
+    }
+
+    /**
+     * Constructor 2
+     * @param n
+     * @param sid
+     * @param em
+     */
+    public Student(String n, String sid, String em) {
+        // TODO:
+		name=n;
+		id=sid;
+		email=em;
+		group="INT2204";
+    }
+
+    /**
+     * Constructor 3
+     * @param s
+     */
+    public Student(Student s) {
+			name=s.getName;
+			id=s.getId;
+			email=s.getEmail;
+			group=s.getGroup;
+    }
+
+    public String getInfo() {
+        // TODO:
+			Symstem.out.println(name + "\n");
+			
+			Symstem.out.println(id + "\n");
+			
+			Symstem.out.println(group + "\n");
+			
+			Symstem.out.println(email + "\n");
+        return null; // xóa dòng này sau khi cài đặt
+    }
+}
