1e736d6f26a4dcd45a0d14795493f832e7ce7da2
leminh2604 <43198814+leminh2604@users.noreply.github.com>
2018-09-25 01:43:36 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,32 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
+        if(b==0) return a;
+        return gcd(b;a%b);
         return 0;
     }
 
     public static int fibonacci(int n) {
+        int f0=0,f1=1;
+        int f=n;
+        if(f==0||f==1){
+            return f;}
+        else if(f>1){
+            for(i=2;i<=f;i++){
+                
+                n=f0+f1;
+                f0=f1;
+                f1=n;
+            }
+        }             
+                
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        return n;
+        
     }
+    public static void main(String ard[])
+	{
+		//System.out.println(gcd(30,12));
+		//System.out.println(fibonacci(3));
+	}
 }
