e5dfd5a1953c78e8628e8bc07651714eb89d4b41
leminh2604 <43198814+leminh2604@users.noreply.github.com>
2018-09-30 04:43:46 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,25 +1,41 @@
 package week3;
 
-public class Week3 {
-
-    public static int max(int m, int n) {
+public class Week3{
+    public static int max(int m,int n){
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if(m>n)
+            return m;
+        else 
+            return n;
     }
 
-    public static int minOfArray(int[] array) {
+    public static int minOfArray(int[] array){
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int min=array[0];
+        for(int i=0;i<array.length;i++){
+            if(array[i]<min){
+                min=array[i];
+            }
+        }
+        return min;
     }
-
-    /**
-     * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
-     * @param weight cân nặng
-     * @param height chiều cao
-     * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
-     */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
+        double x= (double) ((weight)/((height)*(height)));
+        if(x<18.5)
+            return "Thieu can";
+        else if(18.5<=x && x<23)
+            return "Binh Thuong";
+        else if(23<=x && x<25)
+            return "Thua Can";
+        else if(x>=25)
+            return "Beo Phi";
         return null;
     }
+    public static void main(String agrs[]){
+        System.out.println(max(21,48));
+        int array[]={12,15,17,2};
+        System.out.println(minOfArray(array));
+        System.out.println(calculateBMI(57,1.73));
+}
 }
